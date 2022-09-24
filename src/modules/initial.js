0ce3729682df0219953bd46ecdca1b2dbc7bc002
guru <gc66357@gmail.com>
2022-09-24 14:07:36 +0530

    edit style

--- src/modules/initial.js
+++ src/modules/initial.js
@@ -24,4 +24,27 @@ let pageLoad = () => {
     let nav = document.createElement('div');
     nav.setAttribute('id','nav');
     header.appendChild(nav);
+    
+    //nav elements
+    let navElm = [];
+    let navPat = document.createElement('ul');
+    navPat.setAttribute('id','navPat');
+    for(let i=0; i<3; i++)
+    {
+        navElm[i] = document.createElement('li');
+        navElm[i].setAttribute('class','navElm');
+        if(i==0)
+        {
+            navElm[i].innerText="Home";
+        }
+        else if(i==1)
+        {
+            navElm[i].innerText="Menu";
+        }
+        else{
+            navElm[i].innerText="About";
+        }
+        navPat.appendChild(navElm[i]);
+    }
+    nav.appendChild(navPat);
 }
